3d97ea5d5dcdb8cb3f355a5c05a4fe2e3415e531
marvellofaisal <marvellbrazil@gmail.com>
2025-10-12 00:30:09 +0700

    FIXED image height ON component:habitcard AS fixings

--- resources/views/components/habitcard.blade.php
+++ resources/views/components/habitcard.blade.php
@@ -2,7 +2,7 @@
 <div class="w-full lg:w-1/3 habit-card text-white bg-customOrange hover:bg-customBlue p-5 flex flex-col transition-all duration-900 ease-in-out
                         hover:scale-105 hover:rounded-xl hover:shadow-2xl hover:z-10
                         min-h-[480px] hover:min-h-[520px] hover:self-start">
-    <img width="100%" height="100%" class="w-full h-52 md:h-48 hover:h-64 object-cover rounded-lg mb-4"
+    <img width="100%" height="100%" class="w-full h-64 md:h-52 object-cover rounded-lg mb-4"
         src="{{ asset('assets/' . $image) }}" alt="{{ $title }}">
     <h3 class="text-xl md:text-2xl font-bold text-center mb-3">{{ $title }}</h3>
     <p class="text-base text-center cursor-default">
