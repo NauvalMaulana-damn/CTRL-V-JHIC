5c290a1652ab980b922ad07e658d9f49df4d390f
radityapaca02 <radityapaca02@gmail.com>
2025-10-13 20:51:00 +0700

    FIXED bug ON ts/app.ts AS fixings

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -12,7 +12,7 @@ import { initChartGabungan } from "./chart";
 import { initScrollButtons } from "./scroll";
 import MicroModal from "micromodal";
 import "swiper/css";
-// import "swiper/css/navigation";
+import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 // ✅ Mobile menu
