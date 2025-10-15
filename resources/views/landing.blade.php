e9327f0bd9e453724c5178eae1ee181b5746259f
Marvellbrazil <marvellbrazil@gmail.com>
2025-10-15 22:19:17 +0700

    FIXED routes ON view:berita AS revisi

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -17,29 +17,22 @@
             <section class="relative w-full overflow-hidden rounded-xl mb-8">
                 <div class="swiper mySwiper rounded-xl overflow-hidden">
                     <div class="swiper-wrapper" id="x-headnews">
-                        <!-- Hanya gambar -->
-                        <x-headnews image1="default.svg" image2="default.svg" image3="default.svg" />
-                    </div>
-
-                    <!-- Overlay + teks tetap di sini -->
-                    <div
-                        class="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-black/70 to-transparent pointer-events-none">
-                    </div>
-                    <div id="x-headnews-content"
-                        class="absolute bottom-6 left-5 md:left-8 text-white max-w-4xl z-10 pointer-events-none">
-                        <h1 class="title text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold mb-2">
-                            TITLE
-                        </h1>
-                        <p class="desc text-base sm:text-lg md:text-xl lg:text-2xl font-bold mb-6">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet vero dolores aliquam
-                            ab fugiat. Et, nihil voluptatum minus, at minima unde quibusdam temporibus animi dolore
-                            inventore soluta quisquam incidunt obcaecati!
-                        </p>
+                        <x-headnews image="default.svg" title="TITLE">
+                            <x-slot>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure exercitationem excepturi
+                                architecto qui? Exercitationem voluptates tempora distinctio quas eveniet quod
+                                dignissimos, vero deleniti unde laboriosam quasi obcaecati blanditiis mollitia beatae?
+                            </x-slot>
+                        </x-headnews>
                     </div>
 
                     <!-- Pagination & Navigation -->
-                    <div class="swiper-button-prev" style="color: white;"></div>
-                    <div class="swiper-button-next" style="color: white;"></div>
+                    <div class="swiper-button-prev" style="color: white;">
+                        <!-- <i class="fas fa-chevron-left"></i> -->
+                    </div>
+                    <div class="swiper-button-next" style="color: white;">
+                        <!-- <i class="fas fa-chevron-right"></i> -->
+                    </div>
                     <div class="swiper-pagination"></div>
                 </div>
             </section>
@@ -201,26 +194,9 @@
             <div class="bg-white p-5 rounded-xl shadow-md">
                 <h3 class="font-bold text-xl mb-4 border-b-2 border-customOrange pb-2">Berita Terbaru</h3>
                 <div id="x-sidenews" class="space-y-6">
-                    <x-sidenews title="Pendaftaran SKARIGA" image="news-pendaftaran1.webp" onclick="showNews(
-                    'Kuota Pendaftaran SMK PGRI 3 Dibatasi Menjadi 500 Pendaftar',
-                    'Pada Jumat (10/10/2025), SKARIGA dibanjiri para pendaftar. Pendaftaran Berlangsung hingga malam.',
-                    'news-pendaftaran1.webp', 'news-pendaftaran2.webp', 'news-pendaftaran3.jpg')">
-                        Kuota Pendaftaran SMK PGRI 3 Dibatasi Menjadi 500 Pendaftar</x-sidenews>
-                    <x-sidenews title="Juara LKS" image="head_news_1.png" onclick="showNews(
-                    'PARA SISWA MENJUARAI LKS DIKMEN JATIM XXXII',
-                    'BERBAGAI BIDANG MEREKA TAKLUKAN DENGAN POSISI JUARA 1',
-                    'head_news_1.png', 'lksdikmen33rms.jpg', 'lksdikmen33ic.jpg')">
-                        Para Siswa Menjuarai LKS DIKMEN JATIM XXXII</x-sidenews>
-                    <x-sidenews title="Menyambut Hari Kemerdekaan" image="agustusan.jpg" onclick="showNews(
-                    'SAMBUT HARI KEMERDEKAAN, SKARIGA GELAR JALAN SEHAT DAN ANEKA LOMBA PENUH SEMANGAT',
-                    'KEGIATAN BERLANGSUNG DENGAN PENUH SEMANGAT',
-                    'agustusan.jpg', 'agustusan2.jpg', 'agustusan3.jpg')">
-                        Sambut Hari Kemerdekaan, SKARIGA Gelar Jalan Sehat dan Aneka Lomba Penuh Semangat</x-sidenews>
-                    <x-sidenews title="KCS 38" image="kcs38-1.jpg" onclick="showNews(
-                    'PUSDIKARHANUD GALAKKAN GERAKAN KEGIATAN CINTA SEKOLAH UNTUK BENTUK KARAKTER MURID',
-                    'MURID MENGIKUTI DIKLAT DENGAN INTENSIF DAN MENGINAP SATU MALAM DI PUSDIKARHANUD',
-                    'kcs38-1.jpg', 'kcs38-2.jpg', 'kcs38-3.jpg')">
-                        Pusdikarhanud Galakkan Gerakan Kegiatan Cinta Sekolah untuk Bentuk Karakter Siswa</x-sidenews>
+                    @foreach ($beritas as $berita)
+                        <x-sidenews :title="$berita->title" :image="$berita->image" :onclick="" />
+                    @endforeach
                     <a href="/berita">
                         <button
                             class="mt-6 w-full bg-customOrange text-white py-2 rounded-lg font-medium hover:bg-customBlue transition">
