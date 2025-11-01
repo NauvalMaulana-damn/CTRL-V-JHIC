2afc42d483ad30c0649def92851367b5d08ac193
Marvellbrazil <marvellbrazil@gmail.com>
2025-11-01 23:06:37 +0700

    ON PRODUCTION v68

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -1,5 +1,3 @@
-<title>SMK PGRI 3 MALANG | Sekolah Pusat Keunggulan - SKARIGA</title>
-
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
