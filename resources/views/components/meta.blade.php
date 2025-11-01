3c346af8f0f3edbd9482146d77f69bb7ead40061
Marvellbrazil <marvellbrazil@gmail.com>
2025-11-01 23:30:04 +0700

    ON PRODUCTION v69

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="robots" content="index, follow">
 
-<meta name="description" content="SMK PGRI 3 MALANG - Sekolah untuk Kerja, Wirausaha, Kuliah apalagi.">
+<meta name="description" content="Sekolah untuk Kerja, Wirausaha, Kuliah apalagi.">
 <meta name="author" content="Tim CTRL + V SMK PGRI 3 MALANG">
 
 <link rel="canonical" href="https://smkpgri3mlg.web.id">
@@ -11,8 +11,8 @@
 <meta name="google-site-verification" content="zW5OH_Dy5nsOh0tU6KVYqGadqo2e68SXzZr-jiqh5ys" />
 
 <meta property="og:type" content="website">
-<meta property="og:title" content="SMK PGRI 3 MALANG | Sekolah Pusat Keunggulan - SKARIGA">
-<meta property="og:description" content="SMK PGRI 3 MALANG - Sekolah untuk Kerja, Wirausaha, Kuliah apalagi.">
+<meta property="og:title" content="SMK PGRI 3 MALANG">
+<meta property="og:description" content="Sekolah untuk Kerja, Wirausaha, Kuliah apalagi.">
 <meta property="og:image" content="{{ asset('assets/skariga300rbg.png') }}">
 <meta property="og:url" content="https://smkpgri3mlg.web.id">
 
